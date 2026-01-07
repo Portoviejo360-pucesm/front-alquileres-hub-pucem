5b76ddaa89f5d1819c25b4cdf77c34664967ec09
amesitos <uh230839@gmail.com>
2026-01-06 21:57:02 -0500

    merge cambios

--- src/components/layout/PublicTopBar.tsx
+++ src/components/layout/PublicTopBar.tsx
@@ -11,13 +11,14 @@ export default function PublicTopBar() {
   return (
     <header className="header-public">
       {/* Logo */}
-      <Link href="/">
-        <div className="logo-container">
-          {/* Simulamos el icono de ubicación con la P */}
-          <div className="logo-circle">P</div>
-          <span className="logo-text">Portoviejo360</span>
-        </div>
-      </Link>
+          <Link href="/dashboard" className="logo-container">
+            <div className="logo-circle">
+              <svg width="20" height="20" viewBox="0 0 20 20" fill="white">
+                <path fillRule="evenodd" d="M5.05 4.05a7 7 0 119.9 9.9L10 18.9l-4.95-4.95a7 7 0 010-9.9zM10 11a2 2 0 100-4 2 2 0 000 4z" clipRule="evenodd" />
+              </svg>
+            </div>
+            <span className="logo-text">Portoviejo360</span>
+          </Link>
 
       {/* Botones de Acción */}
       <nav className="nav-actions">
